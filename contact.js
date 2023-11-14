8a278a00b775466f21955bbec37a96e8fd0bad58
jonathan <chizzyjonez@gmail.com>
2023-11-14 13:38:30 +0100

    estateSite

---
+++ contact.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#form')
+const userName= document.querySelector('#userName')
+const email = document.querySelector('#email')
+const area = document.querySelector('#area')
+
+document.querySelector(submit, function (e)
+{e.preventDefault();
+});
+
+userNameValue = userName.value.trim()
+emailValue =email.value.trim()
+
+
+if (!userNameValue){
+    
+}
+
+
